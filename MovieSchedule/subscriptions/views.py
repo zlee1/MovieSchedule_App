ee678b3486f598bef07fe6f3e4b6e6282ceb86e1
zack <zack.lee@valpo.edu>
2025-01-08 16:58:31 -0500

    added registration and some css - to be finished later

--- MovieSchedule/subscriptions/views.py
+++ MovieSchedule/subscriptions/views.py
@@ -2,8 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 
-test_profile = {'name': 'Zack', 'zip_codes': [{'zip': '06468', 'active': 1}, {'zip': '10001', 'active': 0}]}
-
 def home(request):
     return render(request, 'subscriptions/home.html')
 
