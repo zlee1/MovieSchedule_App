486cd30b1cff30d82fd9a1c2df7ae3c80c32f1d3
zack <zack.lee@valpo.edu>
2025-01-08 13:59:54 -0500

    linked pages

--- MovieSchedule/subscription/urls.py
+++ MovieSchedule/subscription/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='subscription-home')
+    ,path('home/', views.home, name='subscription-home')
     ,path('login/', views.login, name='subscription-login')
     ,path('profile/', views.profile, name='subscription-profile')
 ]
