8517d66207de08ad2f013a9a626e171899202cf0
zack <zack.lee@valpo.edu>
2025-01-08 13:42:24 -0500

    view populated from model

--- MovieSchedule/subscription/views.py
+++ MovieSchedule/subscription/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Theater, Subscription
+from django.contrib.auth.models import User
 
 test_profile = {'name': 'Zack', 'zip_codes': [{'zip': '06468', 'active': 1}, {'zip': '10001', 'active': 0}]}
 
@@ -10,4 +12,7 @@ def login(request):
     return render(request, 'subscription/login.html')
 
 def profile(request):
-    return render(request, 'subscription/profile.html', context=test_profile)
+    current_user = User.objects.first()
+    context = {'user': current_user, 'subscriptions':current_user.subscription_set.all()}
+    
+    return render(request, 'subscription/profile.html', context=context)
